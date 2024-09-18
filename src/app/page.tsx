19aa724d92c4a680433e6ef6ed0560d08d5a5738
moe toyoda <>
2024-09-18 12:32:21 +0900

    test commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import HomeComponent from "@/components";
+
+export default function Home() {
+  return (
+    <HomeComponent />
+  )
+};
---
+++ src/components/index.tsx
@@ -0,0 +1,46 @@
+'use client'
+import { useState } from "react";
+
+const Home = () => {
+const [text,setText] = useState("")
+const [todos,setTodos] = useState<string[]>([])
+
+const changeText = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+    console.log(text)
+};
+
+const addTodos = () => {
+    const newTodos = [...todos];
+    newTodos.push(text);
+    setTodos(newTodos);
+    setText("");
+};
+
+const deleteTodo = (index: number) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1)
+    setTodos(newTodos);
+};
+
+return (
+    <main>
+            <div>
+                <input type="text" value={text} onChange={changeText} />
+                <button onClick={addTodos}>やることを追加</button>
+            </div>
+            <div>
+                <ul>
+                    {todos.map((todo, index) => (
+                        <li key={todo}>
+                            <p>{todo}</p>
+                            <button onClick={() => deleteTodo(index)}>完了</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+    </main>
+);
+}
+
+export default Home;
